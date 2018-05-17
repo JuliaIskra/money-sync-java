06796447238f7ea332b9be26084e660fb4692f87
Julia Nemtseva <juliaiskra@gmail.com>
2018-05-17 10:29:14 +0300

    Create hello world web app with spring boot

---
+++ src/main/java/juliaiskra/money/sync/hello/Greeting.java
@@ -0,0 +1,22 @@
+package juliaiskra.money.sync.hello;
+
+/**
+ * @author Julia Nemtseva
+ */
+public class Greeting {
+    private final long id;
+    private final String content;
+
+    public Greeting(long id, String content) {
+        this.id = id;
+        this.content = content;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getContent() {
+        return content;
+    }
+}
